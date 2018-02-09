62869684a2a03180cbcefe476b8fb8158985409f
Eric Hertwig <eric@ehdev.de>
2018-02-09 10:40:50 +0100

    Initial Bundles

---
+++ src/Bundle/ProjectBundle/DMKProjectMProjectBundle.php
@@ -0,0 +1,6 @@
+<?php
+namespace DMKProjectM\Bundle\ProjectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DMKProjectMProjectBundle extends Bundle { }
---
+++ src/Bundle/TaskBundle/DMKProjectMTaskBundle.php
@@ -0,0 +1,6 @@
+<?php
+namespace DMKProjectM\Bundle\TaskBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DMKProjectMTaskBundle extends Bundle { }
